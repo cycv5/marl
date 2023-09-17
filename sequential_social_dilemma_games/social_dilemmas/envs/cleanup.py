5f7e0134e65a40b47c41becb2119ee2dcf6cd9bf
Yichen Cai <charlestsai0919@gmail.com>
2023-09-17 16:34:07 -0400

    Full state

--- sequential_social_dilemma_games/social_dilemmas/envs/cleanup.py
+++ sequential_social_dilemma_games/social_dilemmas/envs/cleanup.py
@@ -78,7 +78,8 @@ class CleanupEnv(MapEnv):
                          "other_agent_actions": Box(low=0, high=len(ACTIONS), shape=(self.num_agents - 1, ), dtype=np.int32,),
                          "visible_agents": Box(low=0, high=self.num_agents, shape=(self.num_agents - 1,), dtype=np.int32)})
         else:
-            return Box(low=-np.infty, high=np.infty, shape=(2 * self.view_len + 1, 2 * self.view_len + 1, 3), dtype=np.float32)
+            return Box(low=-np.infty, high=np.infty, shape=(25, 18, 3), dtype=np.float32)
+            # return Box(low=-np.infty, high=np.infty, shape=(2 * self.view_len + 1, 2 * self.view_len + 1, 3), dtype=np.float32)
             # return Box(low=0, high=15, shape=(2 * self.view_len + 1, 2 * self.view_len + 1), dtype=np.int32)
 
     def custom_reset(self):
